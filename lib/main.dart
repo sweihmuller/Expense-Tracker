605fc806bc91069de038982a642e625e8426026e
Santi <sweihmuller01@gmail.com>
2025-07-10 12:39:41 -0300

    Add Material3 theme to the main app for improved UI consistency

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Expenses()));
+  runApp(MaterialApp(theme: ThemeData(useMaterial3: true), home: Expenses()));
 }
