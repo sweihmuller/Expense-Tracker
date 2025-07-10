9f420a713aec38bd352fe6371060bc42a58dd594
Santi <sweihmuller01@gmail.com>
2025-07-10 12:37:47 -0300

    Add AppBar to Expenses widget for improved navigation and user experience

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -28,6 +28,10 @@ class _ExpenseState extends State<Expenses> {
   @override
   Widget build(context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter expense tracker"),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+      ),
       body: Column(
         children: [
           const Text('The chart'),
